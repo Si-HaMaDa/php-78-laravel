c66aa922762f7a59cc1dc30bf06eb7ce5413856e
Si HaMaDa <nrookman_27@yahoo.com>
2022-12-20 22:18:39 +0200

    Add Admin routes file

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\TagsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,39 +20,6 @@ Route::get('/', function () {
 // Route::get('/admin', function () {
 //     return view('admin.index');
 // });
-
-Route::group([
-    'prefix' => 'admin',
-    'as' => 'admin.',
-    'middleware' => ['auth', 'admin'],
-], function () {
-
-    Route::get('/', DashboardController::class)->name('dashboard'); // admin.dashboard
-
-    Route::controller(TagsController::class)->prefix('tags')->as('tags.')->group(function () {
-        Route::get('/', 'index')->name('index'); // admin.tags.index
-
-        Route::get('/create', 'create')->name('create');
-
-        Route::post('/', 'store')->name('store');
-
-        Route::get('/{id}', 'show')->where('id', '[0-9]+')->name('show');
-
-        Route::get('/{id}/edit', 'edit')->name('edit');
-
-        Route::put('/{id}', 'update')->where('id', '[0-9]+')->name('update');
-
-        Route::delete('/{id}', 'destroy')->where('id', '[0-9]+')->name('destroy');
-    });
-
-
-    // Route::resource('categories', CategoryController::class);
-
-    Route::resources([
-        'categories' => CategoryController::class,
-        'posts' => PostController::class
-    ]);
-});
 
 // GET
 // POST
